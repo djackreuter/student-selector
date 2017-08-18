10a9e77f85e08ee3089d867378cbd57d20cf9ca3
Dylan Reuter <djreuter45@gmail.com>
2017-08-18 13:42:19 -0600

    added function and test array

---
+++ period-1/js/script.js
@@ -0,0 +1,14 @@
+var students = [
+		"test1",
+		"test2"
+];
+
+function pickStudent() {
+	var twister = new MersenneTwister();
+	document.getElementById('icon').classList.remove('hidden');
+	setTimeout(function() {
+		document.getElementById('icon').classList.add('hidden');
+		document.getElementById('studentName').innerHTML = students[Math.floor(twister.random() * students.length)];
+	}, 400);
+
+}
